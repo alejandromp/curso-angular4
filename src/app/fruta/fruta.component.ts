e21771b9cc977a3b400c2740dc0a8878ec2b541a
alejandromp <alejandromontoyap@gmail.com>
2018-04-17 06:51:14 -0500

    mi primir commit

---
+++ src/app/fruta/fruta.component.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'fruta',
+    templateUrl: './fruta.component.html'
+})
+
+export class FrutaComponent{
+  public nombre_componente = 'Componente de fruta';
+  public listado_frutas = 'Naranja, Manzana, Pera y sandia';
+
+  public nombre:string;
+  public edad:number;
+  public mayordeEdad:boolean;
+  public trabajos:Array<any> = ['Carpintero', 44, 'Fontanero'];
+  comodin:any;
+
+  constructor(){
+    this.nombre = 'Alejandro Montoya';
+    this.edad = 43;
+    this.mayordeEdad = false;
+    this.comodin ="SI"
+  }
+
+  ngOnInit(){
+      this.cambiarNombre('Adriana Carmona');
+      this.cambiarEdad(45);
+      console.log(this.nombre + " " + this.edad);
+  }
+
+  cambiarNombre(){
+    this.nombre = "Adriana Carmona";
+  }
+  cambiarEdad(edad){
+    this.edad = edad;
+  }
+}
